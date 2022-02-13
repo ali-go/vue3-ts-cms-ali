30319c4848c180761cc8c259db477d65a8f98869
阿离 <1161653413@qq.com>
2022-02-13 20:56:51 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,22 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent
+} from "vue";
+import HelloWorld
+from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
-  },
+
+
+
+
+
+
+  }
 });
 </script>
 
